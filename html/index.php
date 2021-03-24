7120927b8b0202f7250c7a198a5522b101f6303b
justNuka <75932540+justNuka@users.noreply.github.com>
2021-03-24 13:02:43 +0100

    index.php fini

--- html/index.php
+++ html/index.php
@@ -12,17 +12,18 @@
 </head>
 <?php   include("nav.php");     ?>
 <body>
-    <H2 class="mx-6 my-6 fs-0">20 questions aléatoires à chaque quizz</H2>
-        <div class="row">
-            <div class="mx-5 col-6 d-flex justify-content-center">
-                <p class="my-4 ms-5 fs-7">Jusqu'à 60 questions différentes et la possiblité de créer soi-même ses quizzs</p>
-                <button class="boutton1 fs-3 fw-bold btn-outline-danger rounded-pill position-absolute translate-middle">Lancer un quizz</button>
-                <button class="boutton2 fs-3 fw-bold btn-outline-danger rounded-pill position-absolute translate-middle">Créer un quizz</button>
-               
+    <div class="row mt-5" style="width:100%">
+        <div class="col-6 border bg-white rounded-5 mx-6 mt-5">
+            <H2 class="mx-6 my-6 fs-0">20 questions aléatoires à chaque quizz</H2>
+            <p class="my-4 fs-7 mx-6">Jusqu'à 60 questions différentes et la possibilité de créer soi-même ses quizz</p>
+            <div class="mx-5 d-flex mb-5">
+                <button class="btn btn-outline-danger rounded-5 fs-3 fw-bold ms-5 py-2 px-4">Lancer un quizz</button>
+                <button class="btn btn-outline-danger rounded-5 fs-3 fw-bold ms-auto me-5 py-2 px-4">Créer un quizz</button>
             </div>
-            <div class="col-5 position-relative">
-                <img class="rounded mx-auto d-block" src="unknown.png">
-            </div>
-        </div>
+        </div>  
+        <div class="col-5 position-relative">
+            <img class="img-fluid position-absolute top-50 start-50 translate-middle" src="unknown.png">
+        </div>      
+    </div>
 </body>
 </html>
