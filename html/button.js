92d8630341a0b8f6b98839db6e80311612bcf813
Yann-vdv <61906860+Yann-vdv@users.noreply.github.com>
2021-03-25 16:55:46 +0100

    réparation buttons de question

--- html/button.js
+++ html/button.js
@@ -1,7 +1,8 @@
 var select = false;
 function response(id){
     if(select != false){
-        document.getElementById(select).style.Reset();
+        document.getElementById(select).style.background="#f8f9fa";
+        document.getElementById(select).style.color="black";
     }
     document.getElementById(id).style.background="green";
     document.getElementById(id).style.color="white";
